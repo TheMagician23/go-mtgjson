8119cd98e10e5c3ba9aff7dcd5e2a65c2ead1e5a
The Magician <themagician@engineer.com>
2026-01-11 19:56:17 +0000

    Add method to get build metadata

---
+++ getmeta.go
@@ -0,0 +1,33 @@
+package mtgjson
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/TheMagician23/go-mtgjson/datamodels"
+)
+
+type Meta struct {
+	Meta datamodels.FileMeta `json:"meta"`
+	Data datamodels.FileMeta `json:"data"`
+}
+
+const (
+	metaPath = "/Meta.json"
+)
+
+func (c *Client) GetMeta() (Meta, error) {
+	var meta Meta
+
+	r, err := c.API.doRequest(metaPath)
+	if err != nil {
+		return meta, fmt.Errorf("c.API.doRequest: %s", err)
+	}
+
+	err = json.NewDecoder(r).Decode(&meta)
+	if err != nil {
+		return allPrintings, fmt.Errorf("json.NewDecoder(r).Decode: %s", err)
+	}
+
+	return meta, nil
+}
